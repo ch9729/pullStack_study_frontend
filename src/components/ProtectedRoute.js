7e671c57ac864cca65808830b632f31988e46530
ch9729 <karlll@naver.com>
2025-02-11 09:20:24 +0900

    네비게이션 가드

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useMyContext } from "../store/ContextApi";
-
+// prop로 관리자 페이지
 const ProtectedRoute = ({ children, adminPage }) => {
-  // Access the token and isAdmin state by using the useMyContext hook from the ContextProvider
+  // 컨텍스트로 토큰과 어드민페이지 확인
   const { token, isAdmin } = useMyContext();
 
-  //navigate to login page to an unauthenticated
+  // 토큰이 없으면 로그인으로
   if (!token) {
     return <Navigate to="/login" />;
   }
 
-  //navigate to access-denied page if a user try to access the admin page
+  // 토큰이 있고 관리자 페이지 관리자가 아니면
   if (token && adminPage && !isAdmin) {
     return <Navigate to="/access-denied" />;
   }
@@ -20,7 +20,6 @@ const ProtectedRoute = ({ children, adminPage }) => {
 };
 
 export default ProtectedRoute;
-
 
 // USING LOCAL STORAGE OPTION FOR OAUTH ISSUE SINCE IT WAS NOT GETTING REDIRECTED.
 // import React from "react";
