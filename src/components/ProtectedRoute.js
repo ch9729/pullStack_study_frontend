a65aba4eb97b2e79c4ffbfc9382076fba9c4e639
ch9729 <karlll@naver.com>
2025-02-12 11:22:37 +0900

    관리자 권한에게만 목록 보이도록 설정

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -6,12 +6,12 @@ const ProtectedRoute = ({ children, adminPage }) => {
   // 컨텍스트로 토큰과 어드민페이지 확인
   const { token, isAdmin } = useMyContext();
 
-  // 토큰이 없으면 로그인으로
+  // 토큰이 없으면 로그인으로(인증안됨)
   if (!token) {
     return <Navigate to="/login" />;
   }
 
-  // 토큰이 있고 관리자 페이지 관리자가 아니면
+  // 인증된 유저지만 관리자 권한이 없이 관리자 페이지 요청시
   if (token && adminPage && !isAdmin) {
     return <Navigate to="/access-denied" />;
   }
@@ -20,30 +20,3 @@ const ProtectedRoute = ({ children, adminPage }) => {
 };
 
 export default ProtectedRoute;
-
-// USING LOCAL STORAGE OPTION FOR OAUTH ISSUE SINCE IT WAS NOT GETTING REDIRECTED.
-// import React from "react";
-// import { Navigate } from "react-router-dom";
-
-// const ProtectedRoute = ({ children, adminPage = false }) => {
-//   const token = localStorage.getItem('JWT_TOKEN');
-//   const user = JSON.parse(localStorage.getItem('USER'));
-
-//   console.log("ProtectedRoute: Token:", token);
-//   console.log("ProtectedRoute: User:", user);
-
-//   if (!token) {
-//     console.log("ProtectedRoute: No token found, redirecting to login");
-//     return <Navigate to="/login" />;
-//   }
-
-//   if (adminPage && (!user || !user.roles.includes('ADMIN'))) {
-//     console.log("ProtectedRoute: User does not have admin rights, redirecting to access denied");
-//     return <Navigate to="/access-denied" />;
-//   }
-
-//   console.log("ProtectedRoute: Access granted to protected route");
-//   return children;
-// };
-
-// export default ProtectedRoute;
